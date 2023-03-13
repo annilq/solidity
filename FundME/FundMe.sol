8bc25bbf5d2d50e07c979aee92386ac4601023fa
annilq <410493517@qq.com>
2023-03-13 20:33:49 +0800

    update

--- FundME/FundMe.sol
+++ FundME/FundMe.sol
@@ -2,15 +2,13 @@
 
 pragma solidity ^0.8.4;
 import "./PriceConverter.sol";
+error Unauthorized();
 
 /**
- * @title FundMe
+ * @title this title should before contract
  * @dev Store & retrieve value in a variable
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
-// notice custom error only use from solidity 0.8.4
-error Unauthorized();
-
 contract FundMe {
     using PriceConverter for uint256;
     address[] public funders;
